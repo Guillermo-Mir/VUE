dfecb3adcd3e312e1a3652b9d5f7155de9d4fed9
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-10-23 16:25:22 +0200

    Primer commit

---
+++ projecte-blanc/src/reactivitat.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+//definim variable reactiva
+const contador = ref(0) //reactivitat cualquier cosa que modifique lo que se ve por pantalla
+//Funció que modifica el valor
+const incrementar = () => {
+  contador.value++
+}
+
+const missatge = ref('hola, món')
+const canviarMissatge = () => {
+  missatge.value = 'Hola, Vue'
+ }
+
+</script>
+
+<template>
+  <div>
+    <p>El comptaor és: {{contador}}</p>
+    <button @click="incrementar">Incrementar</button>
+  </div>  
+  <div>
+    <h1> {{missatge}}</h1>
+    <button @click="canviarMissatge">Canvia el missatge</button> 
+  </div>
+</template>
+
+<style scoped>
+div{
+  text-align: center;
+}
+button{
+  margin-top: 10px
+}
+</style>

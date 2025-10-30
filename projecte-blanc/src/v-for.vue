f0b8d738242b69b0caf6397169be155919963019
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-10-30 16:58:34 +0100

    Arreglos

--- projecte-blanc/src/v-for.vue
+++ projecte-blanc/src/v-for.vue
@@ -1,12 +1,12 @@
 <template>
     <ul>
-        <li v-for="producte in productes" :key="preoducte.id">
+        <li v-for="producte in productes" :key="producte.id">
             {{producte.nom}} - {{producte.preu}}
         </li>
     </ul>
 </template>
 
-<script>
+<script setup>
     const productes = [
         {id : 1, nom: 'Ordinador', preu: 1000},
         {id : 2, nom: 'Telèfon', preu: 500},

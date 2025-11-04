6d4a485f42eb7f17ee0299fb48ea13d65d3dde76
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-11-04 19:02:01 +0100

    Components-vue

---
+++ components-vue/src/AppTea.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Tea from './components/Tea.vue';
+
+
+</script>
+<template>
+      <div class="content">
+        <h1 class="title">Subscripcions disponibles</h1>
+        <h2 class="subtitle">
+            Disposem dels millor tes del món
+        </h2>
+        <div class="plans">
+            <Tea name="Verds"/>
+            <Tea name="Negres"/>
+            <Tea name="Vermells"/>
+            <Tea name="Blancs"/>
+       </div>
+    </div> 
+</template>
--- projecte-blanc/src/v-on1.vue
+++ projecte-blanc/src/v-on1.vue
@@ -8,5 +8,5 @@
 <template>
     <h1>Hola {{ name }}!</h1>
     <button v-on:click="handleClick('Botó 1')">Click aquí</button>
-    <button @click="handleClick('Boto2')">Click aquí</button>
+    <button @click="handleClick('Botó 2')">Click aquí</button>
 </template>

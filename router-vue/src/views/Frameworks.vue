8811b46ce61992e04ed57e170a46b2fb2e7072c9
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-11-13 17:19:05 +0100

    array de frameworks

--- router-vue/src/views/Frameworks.vue
+++ router-vue/src/views/Frameworks.vue
@@ -1,6 +1,25 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+    const frameworks = ref([
+{ id: 1, name: 'Vue.js', desc: 'Progressive Framework per construir interfícies web.' },
+{ id: 2, name: 'React', desc: 'Biblioteca per crear interfícies d’usuari interactives.' },
+{ id: 3, name: 'Angular', desc: 'Framework complet per aplicacions empresarials.' }
+])
+
+
+</script>
+
 <template>
     <!--
     __________/framework
     -->
     <h1>Frameworks</h1>
+    <ul>
+        <li v-for="frame in frameworks" :key="frame.id">
+            <RouterLink :to="`/frameworks/${frame.id}`"> {{ frame.name }}</RouterLink>
+        </li>
+    </ul>
+    
 </template>

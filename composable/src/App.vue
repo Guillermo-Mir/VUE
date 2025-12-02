218314b60af9fa2a78f346db9f66f8315f897b90
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-12-02 17:34:10 +0100

    Exemple basic

--- composable/src/App.vue
+++ composable/src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+import ExempleBasic from './component/ExempleBasic.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>ExempleBasic</h1>
+  <ExempleBasic></ExempleBasic>
 </template>
 
 <style scoped></style>
---
+++ composable/src/component/ExempleBasic.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useCounter } from '@/composable/useCounter';
+
+const {count, increment, decrement} = useCounter()
+</script>
+
+<template>
+    <div>
+        <p>Compte:{{ count }}</p>
+        <button @click="increment">+</button>
+        <button @click="decrement">+</button>
+    </div>
+</template>

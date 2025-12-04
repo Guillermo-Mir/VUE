655e4c96e3e0da6bd3b9d5e545c92d93caa1a35b
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-12-04 16:02:02 +0100

    Eliminacion de correccion

--- correccio-examen/src/views/TrainingPanel.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-</script>
-
-<template>
-   <h1>Training Panel</h1>
-</template>
-
-<styles scoped>
-</styles>
--- correccio-examen/src/views/TrainingSelector.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-</script>
-
-<template>
-   <h1>Training Selector</h1>
-</template>
-
-<styles scoped>
-</styles>
